e54b616b6203990b2adbeb86a8cf4a0d795b9bab
Julia Lisec <julialisec12@gmail.com>
2020-10-01 05:42:57 +0200

    display initial menu items

--- app.js
+++ app.js
@@ -93,5 +93,5 @@ window.addEventListener('DOMContentLoaded', function() {
    </article>`
  });
  displayMenu = displayMenu.join('');
- console.log(displayMenu);
+ sectionCenter.innerHTML = displayMenu;
 });
