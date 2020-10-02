0e41f1e623ea732b6485f9d08db2c58288fa55a4
Julia Lisec <julialisec12@gmail.com>
2020-10-02 05:43:59 +0200

    get the unique categories

--- app.js
+++ app.js
@@ -87,6 +87,15 @@ const filterBtns = document.querySelectorAll('.filter-btn');
 // load items
 window.addEventListener('DOMContentLoaded', function() {
   displayMenuItems(menu);
+
+  const categories = menu.reduce(function(values, item) {
+    if(!value.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values
+  },
+  ["all"]
+  );
 });
 
 // filter items
